7915a81a62108f701e5a22448704d64c28573687
rouqi <ng.qiqi@live.com>
2024-04-14 19:56:37 +0800

    pizza

--- beautyApp/backend/src/main/java/com/example/beautyApp/controller/SearchController.java
+++ beautyApp/backend/src/main/java/com/example/beautyApp/controller/SearchController.java
@@ -4,7 +4,6 @@ import com.example.beautyApp.manager.SearchManager;
 import com.example.beautyApp.model.ServiceProfile;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
