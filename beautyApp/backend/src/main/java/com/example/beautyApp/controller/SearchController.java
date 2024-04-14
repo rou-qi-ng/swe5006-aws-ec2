b6bfdea28ad462c992c125d389138e2dc8c2b2d5
rouqi <ng.qiqi@live.com>
2024-04-14 19:03:07 +0800

    diu

--- beautyApp/backend/src/main/java/com/example/beautyApp/controller/SearchController.java
+++ beautyApp/backend/src/main/java/com/example/beautyApp/controller/SearchController.java
@@ -4,7 +4,7 @@ import com.example.beautyApp.manager.SearchManager;
 import com.example.beautyApp.model.ServiceProfile;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 // import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
