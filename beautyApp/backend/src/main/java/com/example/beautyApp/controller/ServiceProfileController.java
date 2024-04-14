f58cccf3559856ab0ea70ecc0e3bd9e127a20ccf
rouqi <ng.qiqi@live.com>
2024-04-14 19:23:50 +0800

    lol

--- beautyApp/backend/src/main/java/com/example/beautyApp/controller/ServiceProfileController.java
+++ beautyApp/backend/src/main/java/com/example/beautyApp/controller/ServiceProfileController.java
@@ -8,8 +8,7 @@ import com.example.beautyApp.model.Portfolio;
 import com.example.beautyApp.model.Pricing;
 import com.example.beautyApp.model.Review;
 import com.example.beautyApp.model.ServiceProfile;
-import com.example.beautyApp.model.TB_User;
-import com.example.beautyApp.request.LoginRequest;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.http.HttpStatus;
